d594be31f6186ac045724756ae1336ff87197fd1
shekster21 <p.abhi.p07@gmail.com>
2019-07-04 19:44:28 +0530

    Having trouble with uploading data onto Firebase RTDB.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var firebaseConfig = {
+
+  var firebaseConfig = {
     apiKey: "AIzaSyA6Rn_6EkYbgVMUnSXw1yalqwEDtOzuOSk",
     authDomain: "ledcontroller-3fa45.firebaseapp.com",
     databaseURL: "https://ledcontroller-3fa45.firebaseio.com",
@@ -10,9 +11,49 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  var ledStatus,updates;
   const button = document.querySelector("#Button");
   const outputHeader = document.querySelector("#ledStatus");
-
-  var currLedStatus = firebase.database().ref().child("led_cntroller/leds/led1");
-
+  var Led = firebase.database().ref().child("/led_cntroller/leds");
   
+  
+function myApp(){
+  Led.on("value",function(snapshot){
+    ledStatus = String(snapshot.val().led1);
+    console.log(ledStatus);
+    outputHeader.innerHTML = "LED Status :" + ledStatus;
+    if(ledStatus == "ON"){
+      button.innerHTML = "OFF";
+    }else if(ledStatus == "OFF"){
+      button.innerHTML = "ON";
+    }
+  });
+  button.addEventListener('click',buttonEventHandler());
+
+}
+function buttonEventHandler(){
+  console.log(ledStatus);
+  if(ledStatus == "ON"){
+    updates = {led1:'OFF'};
+    console.log("Swithcing off.");
+  }else if(ledStatus == "OFF"){
+    updates = {led1 :'ON'};
+    console.log("Switching on.");
+  }
+  console.log("button clicked");
+  firebase.database().ref('led_cntroller/leds').update(updates);
+  return;
+}
+
+function getRealTimeValues(){
+  Led.on("value",function(snapshot){
+    ledStatus = String(snapshot.val().led1);
+    console.log(ledStatus);
+    outputHeader.innerHTML = "LED Status :" + ledStatus;
+    if(ledStatus == "ON"){
+      button.innerHTML = "OFF";
+    }else if(ledStatus == "OFF"){
+      button.innerHTML = "ON";
+    }
+  });
+}
